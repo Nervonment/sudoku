6dcfb8869d158a0c8bfacb66979201761c31427a
Nervonment <nervonment@outlook.com>
2024-05-31 21:14:46 +0800

    hidden single

--- src/bin/example/main.rs
+++ src/bin/example/main.rs
@@ -3,19 +3,23 @@ use std::io::{self, stdout};
 use crossterm::{terminal::EnterAlternateScreen, ExecutableCommand};
 use sudoku::{
     game::generator::random_sudoku_puzzle_normal,
-    neo::{puzzle::SudokuPuzzle, techniques::hidden_single},
+    neo::{puzzle::SudokuPuzzle, techniques::{hidden_single_blk, hidden_single_col, hidden_single_row}},
     ui::{draw_grid, draw_numbers},
 };
 
 fn main() -> io::Result<()> {
     let board = random_sudoku_puzzle_normal();
     let puzzle = SudokuPuzzle::new(board);
-    let res = hidden_single(&puzzle);
+    let res_row = hidden_single_row(&puzzle);
+    let res_col = hidden_single_col(&puzzle);
+    let res_blk = hidden_single_blk(&puzzle);
     stdout().execute(EnterAlternateScreen)?;
     draw_grid()?;
     draw_numbers(&board, &board, &[[true; 9]; 9])?;
     println!("");
     println!("");
-    println!("{:?}", res);
+    println!("hidden single in row: {:?}", res_row);
+    println!("hidden single in col: {:?}", res_col);
+    println!("hidden single in blk: {:?}", res_blk);
     Ok(())
 }
--- src/neo/techniques.rs
+++ src/neo/techniques.rs
@@ -1,13 +1,29 @@
-use super::puzzle::{Grid, TrackingCandidates};
+use super::{
+    puzzle::{Grid, TrackingCandidates},
+    utils::block_idx_2_coord,
+};
 
-pub fn hidden_single(puzzle: &(impl TrackingCandidates + Grid)) -> Option<(usize, usize, i8)> {
-    for r in 0..9 {
+fn hidden_single<T, F1, F2>(
+    puzzle: &T,
+    grid_cnt_for_candidate: F1,
+    coord_transform: F2,
+) -> Option<(usize, usize, i8)>
+where
+    T: Grid + TrackingCandidates,
+    F1: Fn(&T, usize, i8) -> i8,
+    F2: Fn(usize, usize) -> (usize, usize),
+{
+    for i in 0..9 {
         for num in 1..=9 {
-            if puzzle.grid_cnt_for_candidate_in_row(r, num) == 1 {
-                let c = (0..9)
-                    .filter(|c: &usize| puzzle.is_candidate_of(r, *c, num))
+            if grid_cnt_for_candidate(puzzle, i, num) == 1 {
+                let j = (0..9)
+                    .filter(|j: &usize| {
+                        let (r, c) = coord_transform(i, *j);
+                        puzzle.is_candidate_of(r, c, num)
+                    })
                     .next()
                     .unwrap();
+                let (r, c) = coord_transform(i, j);
                 if puzzle.is_grid_empty(r, c) {
                     return Some((r, c, num));
                 }
@@ -15,4 +31,28 @@ pub fn hidden_single(puzzle: &(impl TrackingCandidates + Grid)) -> Option<(usize
         }
     }
     None
+}
+
+pub fn hidden_single_row(puzzle: &(impl Grid + TrackingCandidates)) -> Option<(usize, usize, i8)> {
+    hidden_single(
+        puzzle,
+        |p, r, num| p.grid_cnt_for_candidate_in_row(r, num),
+        |r, c| (r, c),
+    )
+}
+
+pub fn hidden_single_col(puzzle: &(impl Grid + TrackingCandidates)) -> Option<(usize, usize, i8)> {
+    hidden_single(
+        puzzle,
+        |p, c, num| p.grid_cnt_for_candidate_in_col(c, num),
+        |c, r| (r, c),
+    )
+}
+
+pub fn hidden_single_blk(puzzle: &(impl Grid + TrackingCandidates)) -> Option<(usize, usize, i8)> {
+    hidden_single(
+        puzzle,
+        |p, b, num| p.grid_cnt_for_candidate_in_blk(b, num),
+        block_idx_2_coord,
+    )
 }
